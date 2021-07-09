062825998b61a133d68f7a1f05a09eb7b70d1407
bhu413 <bhu413@gmail.com>
2021-07-08 23:39:41 -0700

    Version 1.1
    forgot to update actual oven controls

--- backend/src/hardware/oven.js
+++ backend/src/hardware/oven.js
@@ -1,13 +1,16 @@
-const { socket } = require('../../../frontend/src/helpers/socket');
-
-module.exports = function(socketio) {
-    const Gpio = require('pigpio').Gpio;
+module.exports = function(socketio, tempSensor) {
+    
     const Controller = require('node-pid-controller');
-    const TempSensor = require("./temp_sensor");
     const profile = require('../models/profile');
-
+    const Gpio = require('pigpio').Gpio;
     const relay = new Gpio(27, {mode: Gpio.OUTPUT});
     const fan = new Gpio(22, {mode: Gpio.OUTPUT});
+
+    //pid variables
+    var proportional = 0.25;
+    var integral = 0.00;
+    var derivative = 0.00;
+    var dt = 1;
 
     //list of things to export
     var module = {};
@@ -15,67 +18,98 @@ module.exports = function(socketio) {
     var interval;
     var currentProfile;
 
+    //just a linear interpolation function
+    function getTemperatureAtPoint(x) {
+        var datapoints = currentProfile.datapoints;
+        var arrayLength = datapoints.length;
+
+        //finding closest value greater than x
+        var i = 0;
+        if (datapoints[i].x == x) {
+            return datapoints[i].y;  
+        }
+        while (datapoints[i].x < x) {
+            if (i == arrayLength - 1) {
+                return 0;
+            }
+            i++;
+        }
+
+        var x1, x2, y1, y2;
+        if (i == 0) {
+            x1 = 0;
+            y1 = 0;
+        } else {
+            x1 = datapoints[i - 1].x;
+            y1 = datapoints[i - 1].y;
+        }
+        x2 = datapoints[i].x;
+        y2 = datapoints[i].y;
+
+        return y1 + ((x - x1) / (x2 - x1)) * (y2 - y1);
+    }
+
     module.startProfile = function() {
-        var tempHistory = [];
-        var percentage = 0;
-        
         if (currentProfile == null) {
             return -1;
         }
-        var datapoints;
+        var tempHistory = [];
+        var datapoints = currentProfile.datapoints;
+        
+        //need to implement status update for heating, reflow, cooling
         updateStatus("Running");
-        //status update for heating, reflow, cooling
-        datapoints = currentProfile.datapoints;
+    
+        fanOn();
+
+        let ctr = new Controller(proportional, integral, derivative, dt); // k_p, k_i, k_d, dt
         var i = 0;
-        let ctr = new Controller(0.25, 0.01, 0.00, 1); // k_p, k_i, k_d, dt
-
-        ctr.setTarget(datapoints[i]); // 120km/h
-        let correction = ctr.update(TempSensor.getTemp()); // 110km/h is the current speed
-
-        //add percentage for progress bar
-        socketio.emit("historic_temperature_update", {historic_temperature: tempHistory, percentDone: percentage});
-        console.log(correction);
-        correction *= 100;
-        if (correction > 1000) {
-            correction = 1000;
-        }
-        turnRelayOn(correction);
-
-
+        interval = setInterval(() => {
+            ctr.setTarget(getTemperatureAtPoint(i));
+            var correction = ctr.update(tempSensor.getTemp());
+            console.log(correction);
+            turnRelayOn(correction * 100);
+            if (i > datapoints[datapoints.length - 1].x + 30) {
+                module.stop();
+            }
+            tempHistory.push({x: i, y: tempSensor.getTemp()});
+            socketio.emit("historic_temperature_update", {historic_temperature: tempHistory, percent: 0});
+            i++;
+        }, 1000);
         //io emit "user can open door"
         return 0;
     }
-
-
+    
+    
     module.stop = function() {
         clearInterval(interval);
-        relay.pwmWrite(0)
-        fan.digitalWrite(0);
+        relay.digitalWrite(0);
+        fanOff();
         updateStatus("Ready");
     }
-
+    
     module.getStatus = function() {
         return status;
     }
-
+    
     module.getCurrentProfile = function() {
         return currentProfile;
     }
-
+    
     module.loadProfile = function(profileName) {
-        console.log("loading profile");
         currentProfile = profile.getProfile(profileName);
         socketio.emit("new_profile", {current_profile: currentProfile});
     }
-
+    
+    //if less than 50ms, then don't turn on at all
     function turnRelayOn(duration) {
-        if (duration > 1000) {
-            duration = 1000;
+        if (duration >= 1000) {
+            relay.digitalWrite(1);
+        } else if (duration > 50) {
+            relay.digitalWrite(1);
+            setTimeout(function() {
+                relay.digitalWrite(0);
+            }, duration );
         }
-        relay.digitalWrite(1);
-        setTimeout(function() {
-            relay.digitalWrite(0);
-        }, duration );
     }
 
     function fanOn() {
@@ -85,7 +119,7 @@ module.exports = function(socketio) {
     function fanOff() {
         fan.digitalWrite(0);
     }
-
+    
     function updateStatus(newStatus) {
         status = newStatus;
         socketio.emit("status_update", {new_status: status})
@@ -96,6 +130,7 @@ module.exports = function(socketio) {
 
     //if anything goes wrong, stop everything in the oven
     process.on('uncaughtException', (error) => {
+        console.log(error);
         module.stop();
         process.exit(1);
     });
@@ -103,8 +138,7 @@ module.exports = function(socketio) {
     console.log("oven initialized");
 
     return module;
+
 }
-
-
 
 
