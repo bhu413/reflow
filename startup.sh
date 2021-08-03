bbfc1354d3245095ef6a35418e00313c035c6fec
ioerrors <55862966+ioerrors@users.noreply.github.com>
2021-08-03 15:21:39 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,11 +4,7 @@
 cd "$(dirname "$(find / -type d -name backend | head -1)")"
 cd backend
 
-
 #start chrome in kiosk mode and go to correct ip address:port
-DISPLAY=:0.0 chromium-browser --kiosk --app=http://localhost:3001
+DISPLAY=:0.0 chromium-browser --kiosk --app=http://localhost:3001 &
 
 npm start
-
-
-
