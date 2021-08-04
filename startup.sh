1908f41ff2a27d9e97ae2e10750980d0ae0611a8
ioerrors <55862966+ioerrors@users.noreply.github.com>
2021-08-04 10:46:38 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # R-Pi run on start script:
 # 
-sudo cd "$(dirname "$(find / -type d -name backend | head -1)")"
+cd "$(dirname "$(find / -type d -name backend | head -1)")"
 
-sudo cd backend
+cd backend
 
 #start chrome in kiosk mode and go to correct ip address:port
 sudo DISPLAY=:0.0 chromium-browser --kiosk --app=http://localhost:3001 &
